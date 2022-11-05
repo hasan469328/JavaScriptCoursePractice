f4c6bb6c3288b117f930e822b81ff0a553a6a37d
hasan469328 <hasanur.68.bd@gmail.com>
2022-11-05 20:40:23 +0600

    copy and paste strings code from js2 folder

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/* Escaping literal Quotes in Strings */
+const sampleString = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(sampleString);
+
+/* Quoting Strings with Single Quotes */
+
+const string = 'Finn claims to Jack "Algebaric!"';
+console.log(string);
+const goodString = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+console.log(goodString);
+const badString = 'Finn responds, "Let\'s go!"';
+console.log(badString);
+
+/* Escape sequences in Strings */
+const myStrg = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStrg);
+
+/* Concatenating Strings with Plus Operator */
+const mystring = "This is the start. " + "This is the end."
+console.log(mystring);
+
+/* Concatenating Strings with Plus equal Operator */
+let abc = "This is the first sentence. ";
+abc += "This is the second sentence."
+console.log(abc);
+
+/* Constracting strings with Variable */
+const myName = "freecodecamp";
+const abcd = "Hellow, our name is " + myName + ", how are you?"
+console.log(abcd);
+
+/* Appended variables to strings */
+const anAdjective = "awesome!";
+let abcde = "freecodecamp is ";
+abcde += anAdjective;
+console.log(abcde);
