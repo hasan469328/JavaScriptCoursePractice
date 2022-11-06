ab76dd099f6af04367ffb5d4b89323fd673fe1fc
hasan469328 <hasanur.68.bd@gmail.com>
2022-11-06 21:38:35 +0600

    added adding-subtraction-multification-division-practice folder

---
+++ Adding-subtraction-multification-division-practice/js/main.js
@@ -0,0 +1,56 @@
+console.log('Hellow World');
+console.log(typeof 'hasan');
+console.log(typeof 42);
+console.log(typeof true);
+var name = 'hasanur';
+var name = 'rahman';
+console.log(name);
+const FCC = 'freecodecamp'
+let fact = 'is cool!'
+fact = 'is awesome!'
+console.log(FCC, fact);
+
+/* Compound Assignment with Augmented Addition */
+let a = 12;
+let b = 17;
+let c = 10;
+a += 8;
+console.log(a);
+b += 5;
+console.log(b);
+c += 10;
+console.log(c);
+
+/* Compound Assignment with Augmented Subtraction */
+let d = 11;
+let e = 9;
+let f = 3;
+d -= 6;
+console.log(d);
+e -= 15;
+console.log(e);
+f -= 1;
+console.log(f);
+
+/* Compound Assignment with Augmented Multification */
+let g = 5;
+let h = 12;
+let i = 4.6;
+g *= 5;
+console.log(g);
+h *= 3;
+console.log(h);
+i *= 10;
+console.log(i);
+
+/* Compound Assignment with Augmented Division */
+let aa = 48;
+let bb = 108;
+let cc = 33;
+aa /= 12;
+console.log(aa);
+bb /= 4;
+console.log(bb);
+cc /= 11;
+console.log(cc);
+
